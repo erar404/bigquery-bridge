7faa4af72105526f1f63efe797a4ab25b3492850
erar404 <it.arellanoerwin@gmail.com>
2025-12-16 15:57:17 +0800

    code cleanup and slight optimization

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ mssql_database = os.getenv("MSSQL_DATABASE", "default_db")
 mssql_user = os.getenv("MSSQL_USER", "sa")
 mssql_password = os.getenv("MSSQL_PASSWORD", "sqlserver")
 mssql_driver = os.getenv("MSSQL_DRIVER", "ODBC Driver 17 for SQL Server")
-mssql_instance = os.getenv("MSSQL_INSTANCE", "") # project:region:instance-name
+mssql_instance = os.getenv("MSSQL_INSTANCE", "") 
 
 bigquery_project_id = os.getenv("BIGQUERY_PROJECT_ID", "default_project")
 bigquery_dataset_id = os.getenv("BIGQUERY_DATASET_ID", "default_dataset")
--- main.py
+++ main.py
@@ -8,27 +8,19 @@ from core_functions.bigquery_to_mssql import BigQueryToMSSQL
 if __name__ == "__main__":
     # Set up logging
     LogHandler('BigQueryBridge')
+    start_time = datetime.now()
+    logging.info(f"Script started at: {start_time}")
     logger = logging.getLogger('BigQueryBridge')
-
-    # Test Database connections
-    # db_conn = DbConn(logger)
-    # bigquery_conn_str, mssql_conn_str = db_conn.main()
-    # db_conn.close_mssql_connector()
-
-    # logger.info(f'MSSQL Conn: {mssql_conn_str}')
-    # logger.info('------------------------')
-    # logger.info(f'BQ Conn: {bigquery_conn_str}')
 
     # Initialize The Main Function Class
     bq_to_mssql = BigQueryToMSSQL(logger) 
     bq_to_mssql.main()
 
-    # Get current working directory
-    cwd = os.getcwd()
-    logger.info(f"Current working directory: {cwd}")
-
     # Get current date and time
     now = datetime.now()
     formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-    logger.info(f"Current date and time: {formatted_date}")
+    end_time = datetime.now()
 
+    total_time = end_time - start_time
+    logger.info(f"Total execution time: {total_time}")
+    logger.info("Script execution completed.")
